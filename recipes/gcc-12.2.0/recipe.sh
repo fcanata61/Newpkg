1b83227b2aa736010063629b6f86cf967dfce6fe
fcanata61 <fernandocanatta26@gmail.com>
2025-09-06 11:53:05 -0300

    Update recipe.sh

--- recipes/gcc-12.2.0/recipe.sh
+++ recipes/gcc-12.2.0/recipe.sh
@@ -1,5 +1,5 @@
-# Receita do pacote vim
 SRC_URL="https://github.com/vim/vim/archive/refs/tags/v9.1.0000.tar.gz"
-SRC_DIRNAME="vim-9.1.0000+
+SRC_DIRNAME="vim-9.1.0000"
 VERSION="9.1"
 DEPENDS=("ncurses")
+SHA256="0a5c0e1b6b44796c6f21c2edda0abdded0afbd5e1c53d08a0dabe3025d20b8e7"
